6ed47e5861b0d4d9b97494c49ce36a43ec7af345
violetta2413 <71691747+violetta2413@users.noreply.github.com>
2020-10-16 09:38:19 +0200

    Add files via upload

---
+++ f0002c.py
@@ -0,0 +1,7 @@
+#Hány sornyi kiírás, azaz szöveg és üres sor kerül a képernyőre, ha majd egyszer lefuttatod ezt a programot?
+print('Első sor\n')
+print('\nNos?')
+print('Azaz?\n')
+print('\nAkkor most mennyi?\n\n')
+print('Utolsó sor')
+#11 sor szöveg és üresség kerül a képernyőre
