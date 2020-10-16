f391134914b783aa7750edea9a311ae0c5262dad
violetta2413 <71691747+violetta2413@users.noreply.github.com>
2020-10-16 09:34:53 +0200

    Add files via upload

---
+++ f0002b.py
@@ -0,0 +1,12 @@
+#Mi lesz kiírva a program kimenetének utolsó sorában?
+cica = 'kutya'
+print(cica)
+kutya = 'egér'
+egér = 'cica'
+print('kutya')
+cica = 'tyúk'
+print(egér)
+kutya = cica
+print('kutya')
+print(kutya)
+#tyúk lesz kiírva mert kutya=cica és a cica= 'tyúk' a 7. sortól 
